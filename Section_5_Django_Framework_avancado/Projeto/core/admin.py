3444f93c0c42141ab1ebed2c2b36701f2a76d76e
Jamal Andreoti Ghatit <jamalghatit@gmail.com>
2021-05-05 08:35:58 -0300

    aula anterior

--- Section_5_Django_Framework_avancado/Projeto/core/admin.py
+++ Section_5_Django_Framework_avancado/Projeto/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Role, Service, Team
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('services', 'icon', 'active', 'modified')
+    
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = ('role', 'active', 'modified')
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'role', 'active', 'modified')
