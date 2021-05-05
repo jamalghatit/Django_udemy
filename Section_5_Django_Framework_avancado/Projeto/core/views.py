6e23fa015f9205988d2964a57cdeb70b156750ea
Jamal Andreoti Ghatit <jamalghatit@gmail.com>
2021-05-05 12:23:55 -0300

    aula 05/05

--- Section_5_Django_Framework_avancado/Projeto/core/views.py
+++ Section_5_Django_Framework_avancado/Projeto/core/views.py
@@ -1,7 +1,15 @@
 from django.views.generic import TemplateView
 
+from .models import Service, Team
+
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['services'] = Service.objects.order_by('?').all() # faz carregar conteudos aleatório
+        context['team'] = Team.objects.order_by('?').all()
+        return context
 
 class TesteView(TemplateView):
     template_name = 'teste.html'
