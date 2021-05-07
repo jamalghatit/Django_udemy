8b348d20405a2535472e8d2787c7f9dc28f897a3
Jamal Andreoti Ghatit <jamalghatit@gmail.com>
2021-05-07 10:37:24 -0300

    aula 7

--- Section_5_Django_Framework_avancado/Projeto/core/views.py
+++ Section_5_Django_Framework_avancado/Projeto/core/views.py
@@ -1,15 +1,29 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView, TemplateView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 from .models import Service, Team
+from .forms import ContactForm
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class= ContactForm
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['services'] = Service.objects.order_by('?').all() # faz carregar conteudos aleatório
         context['team'] = Team.objects.order_by('?').all()
         return context
+
+    def form_valid(self, form, *args, **kwargs):
+        form.send_email()
+        messages.success(self.request, 'E-mail enviado com sucesso')
+        return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+    def form_invalid(self, form, *args, **kwargs):
+        messages.error(self.request, 'Erro ao enviar e-mail')
+        return super(IndexView, self).form_invalid(form, *args, **kwargs)
 
 class TesteView(TemplateView):
     template_name = 'teste.html'
