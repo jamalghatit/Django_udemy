1231692b28119e93a4b4f524da9271fa1e27d774
Jamal Andreoti Ghatit <jamalghatit@gmail.com>
2021-06-22 16:18:16 -0300

    sessão nove

--- Section_9_Relacionamento_entre_modelos/django_orm/core/models.py
+++ Section_9_Relacionamento_entre_modelos/django_orm/core/models.py
@@ -20,6 +20,10 @@ class Montadora(models.Model):
     def __str__(self):
         return self.nome
 
+def set_default_montadora():
+    return Montadora.objects.get_or_create(nome='Padrão')[0]
+    # Retorna (objeto, boolean)
+
 class Carro(models.Model):
     """
     # OneToOneField
@@ -35,7 +39,7 @@ class Carro(models.Model):
     e um motorista pode dirigir diversos carros.
     """
     chassi = models.OneToOneField('Chassi', on_delete=models.CASCADE)
-    montadora = models.ForeignKey('Montadora', on_delete=models.CASCADE)
+    montadora = models.ForeignKey('Montadora', on_delete=models.SET(set_default_montadora)) #
     motoristas = models.ManyToManyField(get_user_model())
     modelo = models.CharField('Modelo', max_length=32, help_text='Max 30 caracteres')
     preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
