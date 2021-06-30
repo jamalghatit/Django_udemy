8c8a1c34296eac0801ceef53f5dc7011daee42aa
Jamal Andreoti Ghatit <jamalghatit@gmail.com>
2021-06-30 11:04:37 -0300

    redis ok

--- Section_10_Trabalhando_com_Aplicações_em_tempo_real/Projeto/chat/routing.py
+++ Section_10_Trabalhando_com_Aplicações_em_tempo_real/Projeto/chat/routing.py
@@ -3,7 +3,8 @@ from django.urls import re_path # cria path com expressoes regulares
 from .consumers import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<nome_sala>\w+)/$, ChatConsumer'),
+    re_path(r'ws/chat/(?P<nome_sala>\w+)/$', ChatConsumer.as_asgi()),
 ]
+
 
 # ws -> websocket
