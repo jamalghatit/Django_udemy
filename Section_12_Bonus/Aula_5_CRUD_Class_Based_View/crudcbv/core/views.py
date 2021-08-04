807c5e7d01ef17a57f98cf85eeb7422c0cf22f73
Jamal Andreoti Ghatit <jamalghatit@gmail.com>
2021-08-04 10:10:55 -0300

    crud class based view

---
+++ Section_12_Bonus/Aula_5_CRUD_Class_Based_View/crudcbv/core/views.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Produto
+
+
+class IndexView(ListView):
+    models = Produto
+    template_name = 'index.html'
+    queryset = Produto.objects.all() # qual consulta quer fazer no banco de dados.
+    context_object_name = 'produtos'
+    
+class CreateProdutoView(CreateView):
+    model = Produto
+    template_name = 'produto_form.html'
+    fields = ['nome', 'preco']
+    success_url = reverse_lazy('index')
+
+class UpdateProdutoView(UpdateView):
+    model = Produto
+    template_name = 'produto_form.html'
+    fields = ['nome', 'preco']
+    success_url = reverse_lazy('index')
+
+class DeleteProdutoView(DeleteView):
+    model = Produto
+    template_name = 'produto_del.html'
+    success_url = reverse_lazy('index')
